fa0a6f944b47635868164d469da5ffb54955be48
Olagammal Sivasubramanyan <osolagammal@gmail.com>
2022-03-27 21:59:57 +0530

    setup database

---
+++ database/connection.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"todo/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDatabase() *gorm.DB{
+	dbConnectionDetails := "host=localhost user=postgres password=admin dbname=todoGolang port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	connection,err:=gorm.Open(postgres.Open(dbConnectionDetails),&gorm.Config{})
+	if(err!=nil){
+		panic("Sorry! could not connect to database, checck your connection")
+	}
+	DB=connection
+	connection.AutoMigrate(&models.User{})
+	return DB
+}
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"todo/database"
 	"todo/routes"
 )
 
 func main(){
 	allRoutes := routes.SetupRouter()
+	connection:= database.ConnectToDatabase()	
+	fmt.Println(connection)
 	log.Fatal(http.ListenAndServe("127.0.0.1:8000",allRoutes))
 }
